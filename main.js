2052097d5706ff0248224cfa5b6a1ba83dbf612c
cindy19920216 <cindy19920216@gmail.com>
2026-03-24 13:24:14 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Add JS here
+const generateBtn = document.getElementById('generate-btn');
+const lottoNumbersContainer = document.querySelector('.lotto-numbers');
+
+generateBtn.addEventListener('click', () => {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    const spans = lottoNumbersContainer.querySelectorAll('span');
+    spans.forEach((span, index) => {
+        span.textContent = sortedNumbers[index];
+    });
+});
